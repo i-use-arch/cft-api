5688a8d09dce8ad6dd04350d35b5a88a6f12cb50
Sanjay Yepuri <sanjay.yepuri@gmail.com>
2019-11-02 17:27:36 -0500

    added spring webserver

--- src/main/java/xyz/niflheim/cft_api/App.java
+++ src/main/java/xyz/niflheim/cft_api/App.java
@@ -1,11 +1,16 @@
 package xyz.niflheim.cft_api;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import xyz.niflheim.cft_api.db.Mongo;
 import xyz.niflheim.cft_api.db.objects.User;
 import xyz.niflheim.cft_api.db.util.Connection;
 
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
         Mongo mongo = new Mongo(Connection.getDefault(), true, new User());
+
+        SpringApplication.run(App.class, args);
     }
 }
