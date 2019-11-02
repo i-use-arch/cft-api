b8a6d4ab7f88f21e4d54ed91517378f6a5538c5d
David Wang <David-Wang@Outlook.com>
2019-11-02 17:20:29 -0500

    Sanitize application.conf

--- src/main/java/xyz/niflheim/cft_api/AppConfig.java
+++ src/main/java/xyz/niflheim/cft_api/AppConfig.java
@@ -9,7 +9,7 @@ public class AppConfig {
     public static final String MONGO_NAME = CONFIG.getString("Mongo.name");
     public static final String MONGO_HOSTNAME = CONFIG.getString("Mongo.hostname");
     public static final String MONGO_USER = CONFIG.getString("Mongo.user");
-    public static final String MONGO_PASS = CONFIG.getString("Mongo.pass");
+    public static final String MONGO_PASS = System.getProperty("pass");
     public static final String MONGO_OPTIONS = CONFIG.getString("Mongo.options");
 
     public static final int MONGO_PORT = CONFIG.getInt("Mongo.port");
