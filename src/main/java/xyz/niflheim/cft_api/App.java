acac3d44532892ce755020da8ccc308559c30fb7
Sanjay Yepuri <sanjay.yepuri@gmail.com>
2019-11-02 18:25:14 -0500

    added spring amqp dependency

--- src/main/java/xyz/niflheim/cft_api/App.java
+++ src/main/java/xyz/niflheim/cft_api/App.java
@@ -5,12 +5,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import xyz.niflheim.cft_api.db.Mongo;
 import xyz.niflheim.cft_api.db.objects.User;
 import xyz.niflheim.cft_api.db.util.Connection;
+import xyz.niflheim.cft_api.queue.RQueue;
 
 @SpringBootApplication
 public class App {
-    public static void main(String[] args) {
+
+    private final static String QUEUE_NAME = "hello";
+
+
+    public static void main(String[] args) throws Exception {
         Mongo mongo = new Mongo(Connection.getDefault(), true, new User());
 
         SpringApplication.run(App.class, args);
+
+//        RQueue workQueue = new RQueue("localhost", "test");
+//
+//        workQueue.sendMessage("hello, world");
+//
+//        workQueue.registerDeliverCallback(((consumerTag, message) -> {
+//            String response = new String(message.getBody(), "UTF-8");
+//            System.out.println("Recieved message from work queue " + message);
+//        }));
     }
 }
